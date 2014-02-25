9cdbf1c955958caaa565cf66334de230bd87b9f1
frankcash <cfcash@mtu.edu>
2014-02-24 23:56:25 -0500

    Changed it so links are added to an array. Need to change to hashmap in the future.  Just testing things out.

--- mainScraper.py
+++ mainScraper.py
@@ -11,6 +11,8 @@ def runSoup(urlInput):
     content=urllib2.urlopen(url).read()
     soup=BeautifulSoup(content)
     print ("The title of this page is "+ soup.title.string+"\n")
+    links=[ ]
     for link in soup.find_all('a'):
-        print(link.get('href'))
+        print(link.get('href')) # prints all the links in a beautiful way
+        links+=[link.get('href')] # adds all the links to an array
 runSoup(urlInput)
