bf654d614d66c9dba92fe0596dd29f8e0e8ef813
frankcash <cfcash@mtu.edu>
2014-02-25 23:51:41 -0500

    Fixed it so instead of adding to an array, added to a hashmap (dictionary)

--- mainScraper.py
+++ mainScraper.py
@@ -1,3 +1,5 @@
+# Goes to a web site, grabs all the links.  Loops them into a hashmap while printing them individually.
+
 from bs4 import BeautifulSoup
 import urllib
 import urllib2
@@ -11,8 +13,10 @@ def runSoup(urlInput):
     content=urllib2.urlopen(url).read()
     soup=BeautifulSoup(content)
     print ("The title of this page is "+ soup.title.string+"\n")
-    links=[ ]
+    links={}
+    i=0
     for link in soup.find_all('a'):
         print(link.get('href')) # prints all the links in a beautiful way
-        links+=[link.get('href')] # adds all the links to an array
+        links[i]=link.get('href')
+        i+=1
 runSoup(urlInput)
